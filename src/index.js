ec82131b4d66e8272a6777fa9fc1de627d6cf3f6
Andrii Kondysiuk <kondysiuka@gmail.com>
2021-02-22 03:22:06 +0100

    start code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './styles.css';
+import templates from './templates/gallery.hbs'
+import items from './menu.json'
+
+
+
+// const markup = templates(items)
+
+const galleryRef = document.querySelector('.js-menu')
+console.log(galleryRef);
+// galleryRef.insertAdjacentHTML('afterbegin', markup)
