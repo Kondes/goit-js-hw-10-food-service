0e25b4027a76776a87d34e88e837733fd0c4eef4
Andrii Kondysiuk <kondysiuka@gmail.com>
2021-02-23 01:21:40 +0100

    ok

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import './styles.css';
+import templates from './templates/gallery.hbs';
+import items from './menu.json';
+
+// add HTML
+const markup = templates(items);
+const galleryRef = document.querySelector('.js-menu');
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
+
+// add theme
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const changeTheme = document.querySelector('body');
+const buttonTheme = document.querySelector('#theme-switch-toggle');
+
+buttonTheme.addEventListener('change', switchTheme);
+
+function switchTheme() {
+  if (buttonTheme.checked) {
+    changeTheme.classList.remove(Theme.LIGHT);
+    changeTheme.classList.add(Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    changeTheme.classList.remove(Theme.DARK);
+    changeTheme.classList.add(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+}
+
+function restartTheme() {
+  if (localStorage.getItem('theme') === Theme.DARK) {
+    changeTheme.classList.add(Theme.DARK);
+    buttonTheme.checked = true;
+  }
+}
+restartTheme();
