56cbabe68cb590d44a851f1c402649ad9854a2e2
Andrii Kondysiuk <kondysiuka@gmail.com>
2021-02-23 18:31:07 +0100

    upd

--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
 import './styles.css';
-import templates from './templates/gallery.hbs'
-import items from './menu.json'
+import templates from './templates/gallery.hbs';
+import items from './menu.json';
 
+// add HTML
+const markup = templates(items);
+const galleryRef = document.querySelector('.js-menu');
 
+galleryRef.insertAdjacentHTML('beforeend', markup);
 
-// const markup = templates(items)
+// add theme
 
-const galleryRef = document.querySelector('.js-menu')
-console.log(galleryRef);
-// galleryRef.insertAdjacentHTML('afterbegin', markup)
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const changeTheme = document.querySelector('body');
+const buttonTheme = document.querySelector('#theme-switch-toggle');
+
+buttonTheme.addEventListener('change', switchTheme);
+
+function switchTheme() {
+  if (buttonTheme.checked) {
+    changeTheme.classList.remove(Theme.LIGHT);
+    changeTheme.classList.add(Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    changeTheme.classList.remove(Theme.DARK);
+    changeTheme.classList.add(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+}
+
+function restartTheme() {
+  if (localStorage.getItem('theme') === Theme.DARK) {
+    changeTheme.classList.add(Theme.DARK);
+    buttonTheme.checked = true;
+  }
+}
+restartTheme();
